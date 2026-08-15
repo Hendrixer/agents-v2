bbadff9819f26f2453e6728b758c45bb3265bd9c
ekagrashharma <ekagrashharma@gmail.com>
2026-08-15 09:01:13 +0000

    added new google gemini api key 3.7 flash ( working)

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,11 +1,11 @@
+import "dotenv/config";
 import { generateText, type ModelMessage } from "ai";
-import { openai } from "@ai-sdk/openai";
-
+import { google } from "@ai-sdk/google";
 import { SYSTEM_PROMPT } from "./system/prompt.ts";
 
 import type { AgentCallbacks } from "../types.ts";
 
-const MODEL_NAME = "gpt-5-mini";
+const MODEL_NAME = "gemini-3.7-flash";
 
 export async function runAgent(
   userMessage: string,
@@ -13,10 +13,11 @@ export async function runAgent(
   callbacks: AgentCallbacks,
 ): Promise<any> {
   const { text } = await generateText({
-    model: openai(MODEL_NAME),
+    model: google(MODEL_NAME),
     prompt: userMessage,
     system: SYSTEM_PROMPT,
   });
 
   console.log(text);
 }
+runAgent("hello my name is ekagra")
