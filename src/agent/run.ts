a519bb6261b46a375e891c4d3ec11bf3b8fe033e
ekagrashharma <ekagrashharma@gmail.com>
2026-08-17 09:24:00 +0000

    laminar ai added ,

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { generateText, type ModelMessage } from "ai";
 import { tools } from "./tools/index.ts";
+import { Laminar, getTracer } from "@lmnr-ai/lmnr";
 import { executeTools } from "./executeTools.ts";
 import { google } from "@ai-sdk/google";
 import { SYSTEM_PROMPT } from "./system/prompt.ts";
@@ -9,23 +10,26 @@ import type { AgentCallbacks } from "../types.ts";
 
 const MODEL_NAME = "gemini-3.7-flash";
 
+Laminar.initialize({ projectApiKey: process.env.LMNR_PROJECT_API_KEY })
+
 export async function runAgent(
   userMessage: string,
   conversationHistory: ModelMessage[],
   callbacks: AgentCallbacks,
 ): Promise<any> {
-  const { text , toolCalls} = await generateText({
+  const { text } = await generateText({
     model: google(MODEL_NAME),
     prompt: userMessage,
     system: SYSTEM_PROMPT,
     tools,
+    experimental_telemetry : {
+      isEnabled: true,
+      tracer : getTracer()
+    }
   });
 
 
-  console.log(text , toolCalls);
+  console.log("done");
 
-  toolCalls.forEach(async(tc)=>{
-    console.log( await executeTools(tc.toolName , tc.input))
-  })
 }
 runAgent("what is the current time")
